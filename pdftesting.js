1902e14172eddcef5433ca68221ebe531d9ea6b2
Mateeeen <mateen.thefastech@gmail.com>
2024-12-30 13:43:10 +0500

    second commit

--- pdftesting.js
+++ pdftesting.js
@@ -7,7 +7,7 @@ setTimeout(() => {
     container: "#pspdfkit",
     document: "document.pdf",
     initialViewState: new PSPDFKit.ViewState({
-      currentPageIndex: 4, // Page numbers are zero-based, so page 5 is index 4.
+      currentPageIndex: 3, // Page numbers are zero-based, so page 5 is index 4.
     }),
   })
     .then(function (instance) {
