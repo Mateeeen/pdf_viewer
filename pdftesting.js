817016d71ca30cfa89db46cc96dc78af116fdc59
Mateeeen <mateen.thefastech@gmail.com>
2024-12-30 19:59:52 +0500

    second commit

--- pdftesting.js
+++ pdftesting.js
@@ -1,11 +1,11 @@
 setTimeout(() => {
-  const baseUrl = "https://images.app-pursuenetworking.com/public/files/OOPSessional2.pdf";
+  const baseUrl = "https://pdf-viewer-orpin.vercel.app/Assets/";
   console.log(baseUrl);
 
   PSPDFKit.load({
     baseUrl,
     container: "#pspdfkit",
-    document: "document.pdf",
+    document: "https://images.app-pursuenetworking.com/public/files/OOPSessional2.pdf",
     initialViewState: new PSPDFKit.ViewState({
       currentPageIndex: 2, // Page numbers are zero-based, so page 5 is index 4.
     }),
