620f8c757556ff03052bffcc5244eed41db77ae8
Mateeeen <mateen.thefastech@gmail.com>
2024-12-30 20:45:00 +0500

    second commit

--- pdftesting.js
+++ pdftesting.js
@@ -68,7 +68,7 @@ const loadPdfWithPage = (currentPage) => {
         container: "#pspdfkit",
         document: documentUrl,
         initialViewState: new PSPDFKit.ViewState({
-          currentPageIndex: currentPage ? currentPage - 1 : 2, // Adjust for zero-based index
+          currentPageIndex: currentPage, // Adjust for zero-based index
         }),
       })
         .then(function (instance) {
