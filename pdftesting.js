aea46a7d801d67aead9f5f6b3f7f4d51cc628963
Mateeeen <mateen.thefastech@gmail.com>
2024-12-30 13:40:27 +0500

    second commit

--- pdftesting.js
+++ pdftesting.js
@@ -1,17 +1,19 @@
+setTimeout(() => {
+  const baseUrl = "https://pdf-viewer-orpin.vercel.app/Assets/";
+  console.log(baseUrl);
 
-setTimeout(()=>{
-    const baseUrl = "https://pdf-viewer-orpin.vercel.app/Assets/";
-    console.log(baseUrl)
-
-    PSPDFKit.load({
-        baseUrl,
-        container: "#pspdfkit",
-        document: "document.pdf",
-      })
-        .then(function(instance) {
-          console.log("PSPDFKit loaded", instance);
-        })
-        .catch(function(error) {
-          console.error(error.message);
-        });
-},5000)
+  PSPDFKit.load({
+    baseUrl,
+    container: "#pspdfkit",
+    document: "document.pdf",
+    initialViewState: new PSPDFKit.ViewState({
+      currentPageIndex: 4, // Page numbers are zero-based, so page 5 is index 4.
+    }),
+  })
+    .then(function (instance) {
+      console.log("PSPDFKit loaded", instance);
+    })
+    .catch(function (error) {
+      console.error(error.message);
+    });
+}, 5000);
