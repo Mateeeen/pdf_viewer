6a78c2c28b7566bb8144bd8f61054ff243eb4a74
Mateeeen <mateen.thefastech@gmail.com>
2025-01-16 15:37:58 +0500

    third commit

--- pdftesting.js
+++ pdftesting.js
@@ -191,29 +191,37 @@ const loadPdfWithPage = (currentPage) => {
             }
 
 
-              // setTimeout(()=>{
-              //   let commentInfo = JSON.parse(localStorage.getItem("commentInfo"))
-              //   let annotation;
-              //   annotation = new PSPDFKit.Annotations.HighlightAnnotation({
-              //     pageIndex: commentInfo.pageIndex,
-              //     rects: PSPDFKit.Immutable.List([new PSPDFKit.Geometry.Rect(commentInfo.position)]),
-              //     color: new PSPDFKit.Color({ r: 255, g: 255, b: 0 }), // Yellow color
-              //     opacity: 0.5, // 50% opacity
-              //     id: commentInfo.rootId,
-              //   });
+              setTimeout(()=>{
+                let commentInfo = JSON.parse(localStorage.getItem("commentInfo"))
+                let annotation;
+                annotation = new PSPDFKit.Annotations.HighlightAnnotation({
+                  pageIndex: commentInfo.pageIndex,
+                  rects: PSPDFKit.Immutable.List([
+                    new PSPDFKit.Geometry.Rect({
+                      left: 100,
+                      top: 100,
+                      width: 200,
+                      height: 20
+                    })
+                  ]),
+                  color: new PSPDFKit.Color({ r: 255, g: 255, b: 0 }), // Yellow color
+                  opacity: 0.5, // 50% opacity
+                  id: commentInfo.rootId,
+                  text: commentInfo.text,
+                });
 
-              //   const commentAnnotation = new PSPDFKit.Annotations.CommentMarkerAnnotation({
-              //     pageIndex: commentInfo.pageIndex,
-              //     text: commentInfo.text,
-              //     rects: PSPDFKit.Immutable.List([new PSPDFKit.Geometry.Rect(commentInfo.position)]),
-              //     creatorName: commentInfo.creatorName,
-              //     createdAt: new Date(commentInfo.createdAt),
-              //     id: commentInfo.id
-              //   });
-              //   instance.create(annotation);
-              //   instance.create(commentAnnotation);
-              //   console.log("created")
-              // },4000)
+                // const commentAnnotation = new PSPDFKit.Annotations.CommentMarkerAnnotation({
+                //   pageIndex: commentInfo.pageIndex,
+                //   text: commentInfo.text,
+                //   rects: PSPDFKit.Immutable.List([new PSPDFKit.Geometry.Rect(commentInfo.position)]),
+                //   creatorName: commentInfo.creatorName,
+                //   createdAt: new Date(commentInfo.createdAt),
+                //   id: commentInfo.id
+                // });
+                instance.create(annotation);
+                // instance.create(commentAnnotation);
+                console.log("created")
+              },4000)
 
             // setInterval(() => {
             //   instance.getComments().then(function (comments) {
