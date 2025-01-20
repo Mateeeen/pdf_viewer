8ac2eb75daa64c551760f8de055a16ba410361a8
Mateeeen <mateen.thefastech@gmail.com>
2025-01-20 20:18:14 +0500

    fourth commit vercel error 20/01/2025 edit

--- pdftesting.js
+++ pdftesting.js
@@ -73,7 +73,7 @@ const loadPdfWithPage = (currentPage,comments, creatorName) => {
           CommentAvatar: (comment) => {
             let avatar = null
             let allComments = JSON.parse(localStorage.getItem("allComments"))
-            for(x = 0; x <= allComments.length; x++){
+            for(let x = 0; x <= allComments.length; x++){
               if(comment.content == allComments[x].text){
                 avatar = allComments[x].avatar
               }
